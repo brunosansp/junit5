e40a60f37d5fc7fc014e7f6a6b75f7c9e47f40e6
brunosansp <brunoadsjp@gmail.com>
2024-06-01 16:19:51 -0300

    create project and first commit

---
+++ src/main/java/br/com/brunosan/junit5_curso/Calculadora.java
@@ -0,0 +1,20 @@
+package br.com.brunosan.junit5_curso;
+
+public class Calculadora {
+    
+    public int soma(int a, int b) {
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        
+        System.out.println(calculadora.soma(2, 3));
+        System.out.println(calculadora.soma(3, 4));
+        System.out.println(calculadora.soma(5, 6));
+        
+        System.out.println(calculadora.soma(2, 3) == 5);
+        System.out.println(calculadora.soma(3, 4) == 7);
+        System.out.println(calculadora.soma(5, 6) == 11);
+    }
+}
---
+++ src/main/java/br/com/brunosan/junit5_curso/Junit5CursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunosan.junit5_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5CursoApplication.class, args);
+	}
+
+}
